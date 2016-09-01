f5ba08f64478dfc2f0b40d13b339fb05789de9fa
Po-Nan Li <lishantreasure@gmail.com>
2016-08-31 22:01:53 -0700

    init

---
+++ reformat_cvs.rb
@@ -0,0 +1,37 @@
+# encoding=utf-8 
+require 'csv'
+
+lines = STDIN.read #.split("\n")
+
+if ARGV.length > 0
+	filename = ARGV[0]
+else
+	filename = "output"
+end
+
+pat = /([\u4e00-\u9fa5]{0,4})[,]?([\u4e00-\u9fa5]{3,}),[,]?(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+.\d+),(\d+.\d+)/
+
+data = lines.scan(pat).to_a
+
+new_data = []
+
+# puts data
+
+district = ""
+
+for datum in data
+	if datum[0].size > 0
+		district = datum[0]
+	else 
+		datum[0] = district
+	end
+	puts datum[0] + '/' + datum[1]
+	new_data << datum 
+end
+
+CSV.open(filename+".csv", "w") do |csv|
+  csv << ["","","納稅單位","綜合所得總額","平均數","中位數","第一分位數","第三分位數","標準差","變異係數"]  
+  for datum in new_data
+  	csv << datum
+  end
+end
